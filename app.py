ca7091069240ce1227224854630efb7a42bbe841
ZarubinArtem <azarubin@me.com>
2017-04-25 23:27:17 +0300

    add html template

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 # from flask import request
 
 app = Flask(__name__)
@@ -8,26 +9,17 @@ app = Flask(__name__)
 @app.route('/<name>')
 def index(name="Josh"):
     # name = request.args.get('name', name)
-    return "It's work, {0}".format(name)
+    context = {"name":name}
+    return render_template("index.html", **context)
+
 
 @app.route('/add/<int:num1>/<int:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    # return "{} + {} = {}".format(num1, num2, num1+num2)
-    return """
-<!doctype html>
-<html>
-
-    <head><title> Adding stuff </title></head>
-
-    <body>
-        <h1> {} + {} = {} </h1>
-    </body>
-
-</html>
-""".format(num1, num2, num1+num2)
+    context = {'num1':num1, 'num2':num2, 'answer':num1+num2}
+    return render_template("add.html", **context)
 
 
 # run the app
